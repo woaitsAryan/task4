28bd319fc98fea4b2146ef36da5ffc136d2a4960
Aryan Bharti <yoprince.ab@gmail.com>
2023-03-18 18:41:15 +0530

    final changes

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import json
 from cs50 import SQL 
 import pandas as pd
 from importnb import imports
-import datetime
 
-db = SQL("sqlite:///covid19-india.sqlite")
+db = SQL("sqlite:///covid19-india.sqlite") #connecting the database
 
 app = Flask(__name__)
 
@@ -15,40 +14,36 @@ app = Flask(__name__)
 def index():
     states_options = ['West Bengal', 'Telangana', 'Maharashtra', 'Karnataka', 'Goa', 'Delhi']
     data_options = ['Daily Cases', 'Daily Deaths']
-    return render_template('base.html', states_options=states_options, data_options=data_options)
+    return render_template('base.html', states_options=states_options, data_options=data_options) #displaying the state and data options
 
 
 @app.route('/graph', methods = ["POST"])
 def graph():
-    state = request.form['states']
-    data = request.form['data']
+    state = request.form['states'] #Getting the selected state
+    data = request.form['data'] #Getting the selected datatype
     
     inputquery = {"state" :state, 
                  "data" : data}
    
-    outputquerydict = requests.post("http://127.0.0.1:8080/process", json = inputquery).json()
+    outputquerydict = requests.post("http://127.0.0.1:8080/process", json = inputquery).json()  #Getting an appropriate SQL query from the input data
     outputquery = outputquerydict['sqlquery']
-    plottingdata = db.execute(outputquery)
+    plottingdata = db.execute(outputquery) #Using the SQL query to get the data
     
     inputclean = {"dirtydata" :plottingdata, "datatype" : data}
     
-    cleaneddatajson = requests.post("http://127.0.0.1:8080/clean", json = inputclean)
+    cleaneddatajson = requests.post("http://127.0.0.1:8080/clean", json = inputclean) #Cleaning the data of null values and sorting them
 
     cleaneddata = cleaneddatajson.json()['data']
 
     df = pd.read_json(json.dumps(cleaneddata))
-    df.to_csv('data.csv', index = False)
+    df.to_csv('data.csv', index = False) #Printing the data to a .csv file
     
     with imports("ipynb"): # type: ignore
-        import properties  # type: ignore
+        import properties  # type: ignore 
 
-    properties = properties.run()
-    #save_file = open("static/data1.json", "w")
-    #cleaneddata = eval(str(cleaneddata).replace("\"","'"))
-    #json.dump(cleaneddata, save_file,indent = 6)
-    #save_file.close()
+    properties = properties.run() #Running the .ipynb file's run function to get the properties data
     
-    datatype = outputquerydict['datatype']
+    datatype = outputquerydict['datatype'] 
     
     return render_template('graph.html', data = cleaneddata, average = properties['average'], minvalue = properties['minvalue'], maxvalue = properties['maxvalue'],
-                           mindate = properties['mindate'], maxdate = properties['maxdate'], datatype = datatype)
+                           mindate = properties['mindate'], maxdate = properties['maxdate'], datatype = datatype) #Using all the data to render the graph
